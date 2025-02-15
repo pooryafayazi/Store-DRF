94366123bc4afb120ec78e8b7584195e25eca434
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2025-02-15 10:14:08 +0330

    04.01

--- core/website/urls.py
+++ core/website/urls.py
@@ -5,5 +5,7 @@ app_name ='website'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index' ),
+    path('contact/', views.ContactView.as_view(), name='contact' ),
+    path('about/', views.AboutView.as_view(), name='about' ),
     
 ]
--- core/website/views.py
+++ core/website/views.py
@@ -6,3 +6,10 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     template_name = 'website/index.html'
     
+    
+class ContactView(TemplateView):
+    template_name = 'website/contact.html'
+    
+class AboutView(TemplateView):
+    template_name = 'website/about.html'
+    

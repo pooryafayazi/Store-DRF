446d062771292a18f048a4359536fb0e1d520198
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2025-02-17 16:07:41 +0330

    04.08

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     # path('', include('django.contrib.auth.urls')),
     # path('signup/', views.SignupView.as_view(), name='signup'),
     path('login/', views.LoginView.as_view(), name='login'),
-    # path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
     
 ]

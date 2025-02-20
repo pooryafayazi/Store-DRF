b51b276860dd58ef5aedf36d67c6356893024a0d
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2025-02-20 20:07:10 +0330

    05.11

--- core/shop/views.py
+++ core/shop/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.exceptions import FieldError
 from django.views.generic import (
     TemplateView,
     ListView,
@@ -11,12 +12,33 @@ from .models import ProductModel, ProductStatusType, ProductCategoryModel, Prodo
 
 class ShopProductGridView(ListView):
     template_name = 'shop/product-grid.html'
-    queryset = ProductModel.objects.filter(status=ProductStatusType.published.value)
     paginate_by = 9
+    
+    def get_paginate_by(self, queryset):
+        return self.request.GET.get('page_size', self.paginate_by)
+    
+    def get_queryset(self):
+        queryset = ProductModel.objects.filter(status=ProductStatusType.published.value)
+        if search := self.request.GET.get('q'):
+            queryset = queryset.filter(title__icontains=search)
+        if category_id := self.request.GET.get('category_id'):
+            queryset = queryset.filter(category__id=category_id)
+        if min_price := self.request.GET.get('min_price'):
+            queryset = queryset.filter(price__gte=min_price)
+        if max_price := self.request.GET.get('max_price'):
+            queryset = queryset.filter(price__lte=max_price)
+        if order_by := self.request.GET.get('order_by'):
+            try:
+                queryset = queryset.order_by(order_by)
+            except FieldError:
+                pass
+            
+        return queryset
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['total_items'] = self.get_queryset().count()
+        context['categories'] = ProductCategoryModel.objects.all()
         return context
 
 
